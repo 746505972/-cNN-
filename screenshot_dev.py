370605e311ae58ddb7a6927956b3cbc7307d943d
746505972 <746505972@qq.com>
2025-03-12 21:37:45 +0800

    version0.0

--- screenshot_dev.py
+++ screenshot_dev.py
@@ -3,10 +3,17 @@ import cv2
 import numpy as np
 import time
 import os
+from PIL import Image, ImageDraw, ImageFont
 
 # 截图区域
-locate1 = {'left': 0, 'top': 60, 'width': 500, 'height': 600}
-locate2 = {'left': 0 + 300, 'top': 60 + 600, 'width': 1000, 'height': 1000}
+locate00 = {'left': 0 + 224, 'top': 896 + 60, 'width': 1840 - 224, 'height': 1080 - 896}
+locate01 = {'left': 0 + 224, 'top': 896 + 60, 'width': 1840 - 224, 'height': 1080 - 896}
+locate02 = {'left': 0 + 224, 'top': 896 + 60, 'width': 1840 - 224, 'height': 1080 - 896}
+locate03 = {'left': 0 + 224, 'top': 896 + 60, 'width': 1840 - 224, 'height': 1080 - 896}
+locate10 = {'left': 754, 'top': 598, 'width': 1140 - 754, 'height': 796 - 598}
+locate11 = {'left': 1134, 'top': 344, 'width': 1498 - 1134, 'height': 590 - 344}
+locate12 = {'left': 804, 'top': 174, 'width': 1128 - 804, 'height': 346 - 174}
+locate13 = {'left': 422, 'top': 340, 'width': 775 - 422, 'height': 588 - 340}
 # 设置截图保存路径
 save_directory = "F:/GameScreenshots"
 if not os.path.exists(save_directory):
@@ -14,14 +21,43 @@ if not os.path.exists(save_directory):
 
 with mss.mss() as sct:
     while True:
-        screenshot1 = sct.grab(locate1)
-        screenshot1 = np.asarray(screenshot1)[:, :, :3]  # 提取 BGR 颜色通道
-        screenshot2 = sct.grab(locate2)
-        screenshot2 = np.asarray(screenshot2)[:, :, :3]  # 提取 BGR 颜色通道
-        resized1 = cv2.resize(screenshot1, (locate1['width'] // 2, 300))  # 缩小图像
-        resized2 = cv2.resize(screenshot2, (locate2['width'] // 2, 300))
-        # 将两个图像水平拼接在一起
-        combined_image = cv2.hconcat([resized1, resized2])
+        hand0 = cv2.resize(np.asarray(sct.grab(locate00))[:, :, :3], (800, 90))
+        river0 = cv2.resize(np.asarray(sct.grab(locate10))[:, :, :3], (200, 200))
+        river1 = cv2.resize(np.asarray(sct.grab(locate11))[:, :, :3], (200, 200))
+        river2 = cv2.resize(np.asarray(sct.grab(locate12))[:, :, :3], (200, 200))
+        river3 = cv2.resize(np.asarray(sct.grab(locate13))[:, :, :3], (200, 200))
+
+        # 将牌河画面水平拼接在一起
+        river = cv2.hconcat([river0, river1, river2, river3])
+        # 创建一个白色矩形区域
+        white_rect0 = np.ones((40, 800, 3), dtype=np.uint8) * 255  # 200是矩形宽度
+        # 将 OpenCV 图像转换为 PIL 图像
+        white_rect_pil = Image.fromarray(cv2.cvtColor(white_rect0, cv2.COLOR_BGR2RGB))
+        # 在白色矩形上添加中文文本
+        draw = ImageDraw.Draw(white_rect_pil)
+        draw.text((0, 10), "我的牌河", font=ImageFont.truetype("simsun.ttc", 20), fill=(0, 0, 0))  # 黑色文本
+        draw.text((200, 10), "下家牌河", font=ImageFont.truetype("simsun.ttc", 20), fill=(0, 0, 0))  # 黑色文本
+        draw.text((400, 10), "对家牌河", font=ImageFont.truetype("simsun.ttc", 20), fill=(0, 0, 0))  # 黑色文本
+        draw.text((600, 10), "上家牌河", font=ImageFont.truetype("simsun.ttc", 20), fill=(0, 0, 0))  # 黑色文本
+        # 将 PIL 图像转换回 OpenCV 图像
+        white_rect0 = cv2.cvtColor(np.array(white_rect_pil), cv2.COLOR_RGB2BGR)
+
+        # 将截图和白色矩形拼接在一起
+        river = cv2.vconcat([white_rect0, river])
+
+        # 创建一个白色矩形区域
+        white_rect = np.ones((40, 800, 3), dtype=np.uint8) * 255  # 200是矩形宽度
+        # 将 OpenCV 图像转换为 PIL 图像
+        white_rect_pil = Image.fromarray(cv2.cvtColor(white_rect, cv2.COLOR_BGR2RGB))
+        # 在白色矩形上添加中文文本
+        draw = ImageDraw.Draw(white_rect_pil)
+        draw.text((0, 10), "我的手牌", font=ImageFont.truetype("simsun.ttc", 20), fill=(0, 0, 0))  # 黑色文本
+        # 将 PIL 图像转换回 OpenCV 图像
+        white_rect = cv2.cvtColor(np.array(white_rect_pil), cv2.COLOR_RGB2BGR)
+
+        # 将截图和白色矩形拼接在一起
+        combined_image = cv2.vconcat([cv2.vconcat([white_rect, hand0]), river])
+
         cv2.imshow("game", combined_image)
 
         key = cv2.waitKey(1) & 0xFF
